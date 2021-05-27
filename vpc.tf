10060c40bcd9e21de4995fa263a39144a0f41740
sobansal1330 <sonub072@gmail.com>
2021-05-27 10:35:05 +0000

    Commit

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,45 @@
-module "Sonu" {
-  source  = "app.terraform.io/mytest--1330-training/network/google"
-  version = "3.2.2"
-  # insert required variables here
-  network_name = "sonu -network"
-  project_id = var.project
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
 
-  subnets = [
+    project_id   = var.project
+    network_name = "sonu-network"
+    routing_mode = "GLOBAL"
+
+   subnets = [
   {
-    subnet_name   = "sonu-subnet"
+    subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+        subnet-02 = []
+    }
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
 }
