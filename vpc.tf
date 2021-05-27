41f8aad9ec22bff10113a157981b4883ef4191b1
sobansal1330 <sonub072@gmail.com>
2021-05-27 10:25:29 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/mytest--1330-training/network/google"
+  version = "3.2.2"
+  # insert required variables here
+  network_name = "sonu -network"
+  project_id = var.project
+
+  subnets = [
+  {
+    subnet_name   = "sonu-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
